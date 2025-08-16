c074c3c2163beec08878574b7ea26333bfcd9657
cvscharles <cheylansherez@gmail.com>
2025-08-16 17:44:51 +0100

    Initial spring boot setup

---
+++ src/main/java/com/github/cvscharles/crypto_portfolio_api/CryptoPortfolioApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.cvscharles.crypto_portfolio_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoPortfolioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoPortfolioApiApplication.class, args);
+	}
+
+}
